47f5df5715353151ccb7ecf453a27f0996b7cab4
Hasnan Hazim <hazimhasnan@gmail.com>
2022-12-12 19:41:23 +0800

    Modify the app.module for the new features

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 /*
-The imports can import all the modules that we have defined in the projects
-we can use this way to bundle up all the controllers and services associated
-the module
+---> The app.module is our top module to be use by the NestFactory
+
+---> The imports can import all the modules that we have defined in the projects
+      we can use this way to bundle up all the controllers and services associated
+      the module
+
+      can also use anything exported service that the imported module exposed 
+      through its 'exports'
 */
 
 /* 
@@ -14,7 +22,7 @@ the module
 */
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, AuthModule, UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
